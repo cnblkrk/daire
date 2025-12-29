9689289a87d42036b93505e86d1b2aeddeb8318d
cnblkrk <canmarlin029@gmail.com>
2025-12-29 10:05:11 +0300

    Add files via upload

---
+++ daire.py
@@ -0,0 +1,8 @@
+r = float(input("YARI ÇAP GİRİNİZ : "))
+pi = 3.14
+daire_alan = pi * (r ** 2) # -> burada ** yıldız karesini alır / *** olsaydı kübünü alacaktı!!!
+daire_cevre = 2 * pi * r
+print("Daire Alanı: ")
+print(daire_alan)
+print("Daire Çevresi: ")
+print(daire_cevre)
